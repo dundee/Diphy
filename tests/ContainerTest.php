f1f55625121e8d74895c893a8cf5fe7f7721dc16
Daniel Milde <daniel@milde.cz>
2011-03-24 13:38:34 +0100

    Added Container test

---
+++ tests/ContainerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace DiphyTest\Builder;
+
+use DiphyTest\TestCase;
+use Diphy\Container;
+
+/**
+ * @author Daniel Milde <daniel@milde.cz>
+ * @group unit
+ */
+class ContainerTest extends TestCase
+{
+	private $config = array(
+
+	);
+
+	private $loaderConfig = array(
+		'namespaces' => array(
+			'DiphyTest' => APP_DIR,
+			'Diphy' => LIBS_DIR,
+			'Foo'   => FOO_DIR,
+		),
+	);
+
+	private $loader;
+
+	public function setUp()
+	{
+		$this->object = new Container();
+	}
+
+	public function testGetService()
+	{
+		$a = new \Foo\A();
+		$this->object->setService('Foo\A', $a);
+
+		$this->assertInstanceOf('Foo\A', $this->object->getService('Foo\A'));
+	}
+}
