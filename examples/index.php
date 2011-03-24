2cffdee2a7bd82b097bbb7f9f024cc8e881599a6
Daniel Milde <daniel@milde.cz>
2011-03-24 23:02:13 +0100

    Updated example to work with Nette

--- examples/index.php
+++ examples/index.php
@@ -1,31 +1,27 @@
 <?php
 
-use Diphy\Loader\SimpleLoader;
+use Nette\Loaders\RobotLoader;
+use Nette\Caching\FileStorage;
 use Diphy\Builder\ComplexBuilder;
 use Diphy\Parser\ScriptParser;
-use Diphy\Cache\NullCache;
+use Diphy\Cache\NetteCache;
+use Diphy\Loader\NetteRobotLoader;
 
 define('APP_DIR', __DIR__);
 define('LIBS_DIR', __DIR__ . '/../lib');
+define('TMP_DIR', __DIR__ . '/../tmp');
 
-require_once(LIBS_DIR . '/Loader/ILoader.php');
-require_once(LIBS_DIR . '/Loader/SimpleLoader.php');
-
-// autoloading configuration
-$loaderConfig = array(
-	'namespaces' => array(
-		'DiphyExample' => APP_DIR,
-		'Diphy' => LIBS_DIR,
-	),
-);
+require_once(LIBS_DIR . '/nette/Nette/loader.php');
 
 // setup autoloading
-$loader = new SimpleLoader($loaderConfig);
-$loader->registerAutoload();
+$loader = new RobotLoader();
+$loader->setCacheStorage(new FileStorage(TMP_DIR));
+$loader->addDirectory(APP_DIR);
+$loader->addDirectory(LIBS_DIR);
+$loader->register();
 
 // create DI container
-$di = new ComplexBuilder(new ScriptParser(), new NullCache());
-$di->registerClassLoader($loader);
-
+$di = new ComplexBuilder(new ScriptParser(), new NetteCache(new FileStorage(TMP_DIR)));
+$di->registerClassLoader(new NetteRobotLoader($loader));
 
 $di->getService('DiphyExample\Application')->run();
